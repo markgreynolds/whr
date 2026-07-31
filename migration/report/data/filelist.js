637e352300cb4b68dc51574c7d2ed03a1f3a58f1
Mark Greynolds <mark.greynolds@sap.com>
2026-07-31 00:26:03 +0000

    First commit of migration artifacts

---
+++ migration/report/data/filelist.js
@@ -0,0 +1,142 @@
+var main_filelist = [
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/AV_HB_ZCOPALIC.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/AV_HB_ZCOPALIC.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 0
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/migration/todo/WHRMIG/BASE/DIM_HB_CUST_SALES.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/migration/todo/WHRMIG/BASE/DIM_HB_CUST_SALES.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 1
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/migration/todo/WHRMIG/BASE/DIM_HB_MATERIAL.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/migration/todo/WHRMIG/BASE/DIM_HB_MATERIAL.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 2
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BASE/DIM_HB_MAT_PLANT_CVTD.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BASE/DIM_HB_MAT_PLANT_CVTD.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 3
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/CV_HB_ZGPSLOD3.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/CV_HB_ZGPSLOD3.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 4
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/CV_HB_ZGPSPPCH.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BASE/FIN/TRANS/CV_HB_ZGPSPPCH.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 5
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BASE/ISC/TRANS/CVTD/CV_HB_SOLD_TO.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BASE/ISC/TRANS/CVTD/CV_HB_SOLD_TO.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 6
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BUS/FIN/COPA/CV_ZCOPALIC.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BUS/FIN/COPA/CV_ZCOPALIC.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 7
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BUS/FIN/ISC/CV_ZPRODH2_HIER_LVL.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BUS/FIN/ISC/CV_ZPRODH2_HIER_LVL.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 8
+  },
+  {
+    "name": "db/home/user/projects/WHRMIG_PROJ/db/src/BUS/ISC/OTR/calculationviews/CV_ZPRODH2_HIER_LVL.hdbcalculationview",
+    "pkgOnlyName": "me/user/projects/WHRMIG_PROJ/db/src/BUS/ISC/OTR/calculationviews/CV_ZPRODH2_HIER_LVL.hdbcalculationview",
+    "ext": "hdbcalculationview",
+    "container": "db",
+    "index": 9
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/FIN/TRANS/AV_HB_ZCOPALIC.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/FIN/TRANS/AV_HB_ZCOPALIC.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 10
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/DIM_HB_CUST_SALES.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/DIM_HB_CUST_SALES.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 11
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/DIM_HB_MATERIAL.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/DIM_HB_MATERIAL.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 12
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/DIM_HB_MAT_PLANT_CVTD.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/DIM_HB_MAT_PLANT_CVTD.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 13
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/FIN/TRANS/CV_HB_ZGPSLOD3.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/FIN/TRANS/CV_HB_ZGPSLOD3.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 14
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/FIN/TRANS/CV_HB_ZGPSPPCH.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/FIN/TRANS/CV_HB_ZGPSPPCH.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 15
+  },
+  {
+    "name": "db/src/WHRMIG/BASE/ISC/TRANS/CVTD/CV_HB_SOLD_TO.properties",
+    "pkgOnlyName": "/WHRMIG/BASE/ISC/TRANS/CVTD/CV_HB_SOLD_TO.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 16
+  },
+  {
+    "name": "db/src/WHRMIG/BUS/FIN/COPA/CV_ZCOPALIC.properties",
+    "pkgOnlyName": "/WHRMIG/BUS/FIN/COPA/CV_ZCOPALIC.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 17
+  },
+  {
+    "name": "db/src/WHRMIG/BUS/FIN/ISC/CV_ZPRODH2_HIER_LVL.properties",
+    "pkgOnlyName": "/WHRMIG/BUS/FIN/ISC/CV_ZPRODH2_HIER_LVL.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 18
+  },
+  {
+    "name": "db/src/WHRMIG/BUS/ISC/OTR/calculationviews/CV_ZPRODH2_HIER_LVL.properties",
+    "pkgOnlyName": "/WHRMIG/BUS/ISC/OTR/calculationviews/CV_ZPRODH2_HIER_LVL.properties",
+    "ext": "properties",
+    "container": "db",
+    "index": 19
+  }
+];
